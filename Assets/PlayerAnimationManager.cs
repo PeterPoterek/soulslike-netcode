4bb463feed5fcbd5b744b3fed4a65ef61426e5a6
Piotr Poterek <peterpoterek0@gmail.com>
2023-09-17 21:16:33 +0200

    Update PlayerAnimationManager.cs

--- Assets/PlayerAnimationManager.cs
+++ Assets/PlayerAnimationManager.cs
@@ -7,7 +7,7 @@ public class PlayerAnimationManager : CharacterAnimationManager
     // Start is called before the first frame update
     void Start()
     {
-
+        // 
     }
 
     // Update is called once per frame
